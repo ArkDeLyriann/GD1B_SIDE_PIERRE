ab125a6d4cfc02a50f5b5f29504253b125fc9e75
PIERREHugo <1hugopierre1@gmail.com>
2023-01-17 17:18:12 +0100

    HELP ME GOD

---
+++ assets/tuiles_de_jeu.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tuiles_de_jeu" tilewidth="32" tileheight="32" tilecount="324" columns="18">
+ <image source="E:/LD/PremierNiveau/tuiles_de_jeu.png" width="600" height="600"/>
+ <tile id="0">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="estSolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
